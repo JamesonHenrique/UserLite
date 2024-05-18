cf02a3c050f01356f11c22fe881a31da61220bfc
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-05-18 17:57:29 -0300

    Modificar DataLoader

--- src/main/java/usermanagement/jdbc/DataLoader.java
+++ src/main/java/usermanagement/jdbc/DataLoader.java
@@ -1,6 +1,5 @@
 package usermanagement.jdbc;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.core.io.ClassPathResource;
@@ -10,6 +9,7 @@ import org.springframework.stereotype.Component;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 
 @Component
 public class DataLoader implements CommandLineRunner {
@@ -21,6 +21,14 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Resource resource = new ClassPathResource("data.sql");
         String sql = new String(Files.readAllBytes(Paths.get(resource.getURI())));
-        jdbcTemplate.execute(sql);
+
+        // Split the SQL string into multiple statements
+        String[] sqlStatements = sql.split(";");
+
+        // Execute each SQL statement
+        Arrays.stream(sqlStatements)
+                .map(String::trim)  // Remove leading and trailing whitespaces
+                .filter(statement -> !statement.isEmpty())  // Ignore empty lines
+                .forEach(jdbcTemplate::execute);
     }
 }
