0c3854ec34193dfd09b6eb09b0f435e21b9649a5
Jameson Henrique <122840154+JamesonHenrique@users.noreply.github.com>
2024-05-18 17:41:23 -0300

    Refatorar tests e codigo

--- springboot-restful-webservices/src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT IGNORE INTO users (id,first_name,last_name,email) VALUES (1, 'Usuario', 'Admin', 'admin@gmail.com' )
-INSERT IGNORE INTO users (id,first_name,last_name,email) VALUES (2, 'Jameson', 'Henrique', 'jameson@gmail.com' )
